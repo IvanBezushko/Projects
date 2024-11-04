86730c2d9a4587eae991ebfc37105ec4d924049e
Ivan Bezushko <150702599+IvanBezushko@users.noreply.github.com>
2024-11-04 10:43:33 +0100

    KanBan-Board code

---
+++ KanBan-Board/Done.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Done() {
+  return (
+    <div className="done-container">
+      <h2>Done</h2>
+      {/* Lista zadań */}
+    </div>
+  );
+}
+
+export default Done;
